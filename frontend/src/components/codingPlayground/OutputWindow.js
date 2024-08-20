3b585535a3ccd162ba282d68034130e125d69f36
Deepak Jhawar <deepakjhawar04@gmail.com>
2024-08-21 01:39:21 +0530

    output window size

--- frontend/src/components/codingPlayground/OutputWindow.js
+++ frontend/src/components/codingPlayground/OutputWindow.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const OutputWindow = ({ outputDetails }) => {
-	const textColor = outputDetails && outputDetails.status === 'F	ailed' ? 'text-red-500' : 'text-white';
+	const textColor = outputDetails && outputDetails.status === 'Failed' ? 'text-red-500' : 'text-white';
 
 	return (
-		<div className="w-full h-full bg-gray-800 p-4 rounded-md shadow-lg overflow-auto">
+		<div className="w-full bg-gray-800 p-4 rounded-md shadow-lg overflow-auto max-h-96">
 			{outputDetails === null || outputDetails.output === null ? (
 				<p className="text-gray-400">Here you see your output</p>
 			) : (
