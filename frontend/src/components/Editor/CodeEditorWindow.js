11f85574afe5e115736e404298dadfa0c136332c
lahariSane <135339666+lahariSane@users.noreply.github.com>
2024-08-30 18:47:32 +0530

    monaco to codemirror

--- frontend/src/components/Editor/CodeEditorWindow.js
+++ frontend/src/components/Editor/CodeEditorWindow.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import Editor from '@monaco-editor/react';
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { cpp } from "@codemirror/lang-cpp";
+import { python } from "@codemirror/lang-python";
+import { javascript } from "@codemirror/lang-javascript";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 
-const CodeEditorWindow = ({ onChange, language, code }) => {
+const languageExtensions = {
+  cpp,
+  python,
+  javascript,
+};
+
+const themes = { vscodeDark };
+
+const CodeEditorWindow = ({
+  onChange,
+  language = "python",
+  code,
+  theme = "vscodeDark",
+}) => {
   const handleEditorChange = (value) => {
-    onChange('code', value);
+    onChange("code", value);
   };
 
   return (
     <div className="w-full h-full bg-gray-900 p-4 rounded-md shadow-lg">
-      <Editor
-        height="85vh"
-        width="100%"
-        language={language || 'python'}
-        value={code}  
+      <CodeMirror
+        value={code}
+        height="75vh"
+        theme={themes[theme]}
+        language={language}
+        extensions={language !== "c" ? [languageExtensions[language]()] : []}
         onChange={handleEditorChange}
-        theme="vs-dark"
         options={{
-          selectOnLineNumbers: true,
-          automaticLayout: true,
+          lineNumbers: true,
+          indentUnit: 4,
+          tabSize: 4,
+        }}
+        basicSetup={{
+          lineNumbers: true,
+          highlightActiveLineGutter: true,
+          highlightSpecialChars: true,
+          history: true,
+          foldGutter: true,
+          drawSelection: true,
+          dropCursor: true,
+          allowMultipleSelections: true,
+          indentOnInput: true,
+          syntaxHighlighting: true,
+          bracketMatching: true,
+          closeBrackets: true,
+          autocompletion: true,
+          rectangularSelection: true,
+          crosshairCursor: true,
+          highlightActiveLine: true,
+          highlightSelectionMatches: true,
+          closeBracketsKeymap: true,
+          defaultKeymap: true,
+          searchKeymap: true,
+          historyKeymap: true,
+          foldKeymap: true,
+          completionKeymap: true,
+          lintKeymap: true,
         }}
       />
     </div>
